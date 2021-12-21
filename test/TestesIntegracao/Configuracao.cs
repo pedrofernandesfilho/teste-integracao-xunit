e7af208d38f5ea1e5dcb93c4e02b148ca03a081c
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2021-12-21 00:31:32 -0300

    Implementa classes de configuração dos testes
    Implementação mínima para subir servidor web de testes

--- test/TestesIntegracao/Configuracao.cs
+++ test/TestesIntegracao/Configuracao.cs
@@ -1,22 +1,43 @@
+using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Net.Http;
 using Xunit;
 
 namespace TesteIntegracaoxUnit.TestesIntegracao;
 
 [CollectionDefinition(Nome)]
-sealed class Colecao {
+public sealed class Colecao : ICollectionFixture<Fixture>
+{
    public const string Nome = nameof(Colecao);
 }
 
-sealed class Fixture : IDisposable
+public sealed class Fixture : IDisposable
 {
+    public WebApplicationFactory<Program> AplicacaoWeb { get; }
+    public HttpClient ClienteHttp { get; }
+
+    public Fixture()
+    {
+        const string urlApi = "http://localhost/api/";
+
+        AplicacaoWeb = new WebApplicationFactory<Program>();
+        ClienteHttp = AplicacaoWeb.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            BaseAddress = new Uri(urlApi)
+        });
+    }
+
     public void Dispose()
     {
+        ClienteHttp.Dispose();
+        AplicacaoWeb.Dispose();
     }
 }
 
 [Collection(Colecao.Nome)]
-class TesteBase
+public class TesteBase
 {
-    
+    public HttpClient ClienteHttp { get; }
+
+    public TesteBase(Fixture fixture) => ClienteHttp = fixture.ClienteHttp;
 }
