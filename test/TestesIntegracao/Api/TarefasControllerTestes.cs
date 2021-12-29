8e24992200f96e4adec2aa022a1b7001e5b1e93f
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2021-12-29 03:02:23 -0300

    Refatora testes do Get

--- test/TestesIntegracao/Api/TarefasControllerTestes.cs
+++ test/TestesIntegracao/Api/TarefasControllerTestes.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using TesteIntegracaoxUnit.WebApi.Models;
@@ -8,34 +10,54 @@ using Xunit;
 
 namespace TesteIntegracaoxUnit.TestesIntegracao.Api;
 
-public class TarefasControllerTestes : TesteBase
+public class TarefasControllerTestes
 {
-    public TarefasControllerTestes(Configuracao fixture) : base(fixture) { }
-
-    [Fact]
-    public async Task DadoQueExistemTarefasCadastradas_QuandoSolicitarUmaTarefaCadastradaInformandoOIdentificador_DeverRetorarAsInformacoesDaTarefa()
+    public class DadoQueExistemTarefasCadastradas_QuandoSolicitarUmaTarefaCadastradaInformandoOIdentificador : TesteBase, IDisposable
     {
-        var tarefaEsperada = new TarefaModel(2, "Tarefa 2 do teste", true);
+        private readonly TarefaModel tarefaEsperada;
+        private HttpResponseMessage respostaHttp = default!;
 
-        var tarefas = ServiceProvider.GetRequiredService<IList<TarefaModel>>();
-        tarefas.Add(new TarefaModel(1, "Tarefa 1 do teste", false));
-        tarefas.Add(tarefaEsperada);
-        tarefas.Add(new TarefaModel(3, "Tarefa 3 do teste", false));
+        public DadoQueExistemTarefasCadastradas_QuandoSolicitarUmaTarefaCadastradaInformandoOIdentificador(Configuracao fixture) : base(fixture)
+        {
+            tarefaEsperada = new TarefaModel(2, "Tarefa 2 do teste", true);
 
-        using var resposta = await ClienteHttp.GetAsync("Tarefas/2");
-        var respostaConteudo = await resposta.Content.ReadAsStringAsync();
-        var tarefaRetornada = JsonSerializer.Deserialize<TarefaModel>(respostaConteudo, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var tarefas = ServiceProvider.GetRequiredService<IList<TarefaModel>>();
+            tarefas.Add(new TarefaModel(1, "Tarefa 1 do teste", false));
+            tarefas.Add(tarefaEsperada);
+            tarefas.Add(new TarefaModel(3, "Tarefa 3 do teste", false));
 
-        Assert.Equal(tarefaEsperada, tarefaRetornada);
+            Task.Run( async () => {
+                respostaHttp = await ClienteHttp.GetAsync("Tarefas/2");
+            }).Wait();
+        }
+
+        [Fact]
+        public async Task DeverRetorarAsInformacoesDaTarefa()
+        {
+            var respostaConteudo = await respostaHttp.Content.ReadAsStringAsync();
+            var tarefaRetornada =
+                JsonSerializer.Deserialize<TarefaModel>(respostaConteudo,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                );
+
+            Assert.Equal(tarefaEsperada, tarefaRetornada);
+        }
+
+        [Fact]
+        public void DeveRetornarEstadoOk() =>
+            Assert.Equal(HttpStatusCode.OK, respostaHttp.StatusCode);
+
+
+        public void Dispose() => respostaHttp.Dispose();
     }
 
-    [Fact]
-    public async Task DadoQueExistemTarefasCadastradas_QuandoSolicitarUmaTarefaNaoCadastradaInformandoOIdentificador_DeverRetorarNaoEncontrado()
-    {
-        const HttpStatusCode respostaEsperada = HttpStatusCode.NotFound;
+    // [Fact]
+    // public async Task DadoQueExistemTarefasCadastradas_QuandoSolicitarUmaTarefaNaoCadastradaInformandoOIdentificador_DeverRetorarNaoEncontrado()
+    // {
+    //     const HttpStatusCode respostaEsperada = HttpStatusCode.NotFound;
         
-        using var resposta = await ClienteHttp.GetAsync("Tarefas/20");
+    //     using var resposta = await ClienteHttp.GetAsync("Tarefas/20");
 
-        Assert.Equal(respostaEsperada, resposta.StatusCode);
-    }
+    //     Assert.Equal(respostaEsperada, resposta.StatusCode);
+    // }
 }
